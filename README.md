bf0ec72603eb653ac08b03ae4d020896645f9fad
AyaanGH <25395404+AyaanGH@users.noreply.github.com>
2020-10-26 16:35:49 +0000

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Ray-Tracer
+# C++ Ray-Tracer
+
+
+
+![alt text](render.png "Ray traced sphere")
+
+
+
+## About
+
+C++ Ray Tracer is rendering photorealistic 3D scenes using the path based ray tracing technique.
+Multi threading is also implemented for high performance renders. 
--- main.cpp
+++ main.cpp
@@ -80,9 +80,9 @@ int main() {
 
     //Image metadata
     const double aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
+    const int image_width = 1920;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100;
+    const int samples_per_pixel = 500;
     const int max_depth = 50;
 
     //Camera metadata
